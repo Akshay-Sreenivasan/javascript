c5cc7eb38e00ed6b7d52428979c94e0a13340c08
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-11-10 08:53:21 +0530

    javascript

---
+++ on:\off.js
@@ -0,0 +1,10 @@
+function data()
+{
+    document.getElementById('data').innerHTML="Welcome";
+    document.getElementById('data').style='color:red';
+
+}
+function change()
+{
+    document.getElementById('img1').src
+}
